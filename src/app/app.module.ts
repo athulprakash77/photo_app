452abfc56e4ad0c7f16cf14d343ce1eed21ead0d
athulprakash007 <athull362@gmail.com>
2022-11-20 11:43:20 -0800

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
+import { ViewallphotoComponent } from './viewallphoto/viewallphoto.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myroutes:Routes=[
+  {path:"",component: PhotoComponent},
+  {path:"viewallphoto",component: ViewallphotoComponent}
+
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    ViewallphotoComponent,
+    NavbarComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
